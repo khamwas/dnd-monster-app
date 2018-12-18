158e95a6f03fdef7861aa0e9e53e693222516375
Emmanuel Bravo <eebravo1@gmail.com>
2018-12-18 13:44:43 -0600

    fixed controller

--- server/controllers/monster_controller.js
+++ server/controllers/monster_controller.js
@@ -4,7 +4,7 @@ let battleField = [];
 let monsters = [];
 let id = 326;
 
-axios.get('http://dnd5eapi.co/api/monsters/').then((response) => {
+axios.get('http://www.dnd5eapi.co/api/monsters/').then((response) => {
 	for (let i = 0; i < response.data.results.length; i++) {
 		axios.get(response.data.results[i].url).then((val) => {
 			monsters.push(val.data);
@@ -14,27 +14,21 @@ axios.get('http://dnd5eapi.co/api/monsters/').then((response) => {
 
 module.exports = {
 	getIndex: (req, res, next) => {
-		res
-			.status(200)
-			.json(id)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(id);
+		// .catch((err) => res.status(500).send(err));
 		// .catch(err => res.status(500).send(err));
 	},
 	getMonsters: (req, res, next) => {
-		res
-			.status(200)
-			.json(monsters)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(monsters);
+		// .catch((err) => res.status(500).send(err));
 	},
 
 	postMonsters: (req, res, next) => {
 		monsters.push(req.body);
 		id++;
 		// console.log(monsters[monsters.length - 1]);
-		res
-			.status(200)
-			.json(monsters)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(monsters);
+		// .catch((err) => res.status(500).send(err));
 	},
 	putMonsters: (req, res, next) => {
 		monsters.push(req.body);
@@ -44,10 +38,8 @@ module.exports = {
 		monsters.splice(monsterId, 1);
 		//edit code here
 		// console.log(req.body.index);
-		res
-			.status(200)
-			.json(monsters)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(monsters);
+		// .catch((err) => res.status(500).send(err));
 	},
 	deleteMonsters: (req, res, next) => {
 		// console.log(":" + monsters[0].index);
@@ -56,18 +48,14 @@ module.exports = {
 			(monster) => ':' + monster.index == deleteId
 		);
 		monsters.splice(monsterId, 1);
-		res
-			.status(200)
-			.json(monsters)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(monsters);
+		// .catch((err) => res.status(500).send(err));
 		// console.log('delete successful', monsters.map((elem) => elem.name));
 	},
 
 	getBattleField: (req, res, next) => {
-		res
-			.status(200)
-			.json(battleField)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(battleField);
+		// .catch((err) => res.status(500).send(err));
 	},
 	postBattleField: (req, res, next) => {
 		let pushId = req.params.id;
@@ -75,10 +63,8 @@ module.exports = {
 			(monster) => ':' + monster.index == pushId
 		);
 		battleField.push(monsters[monsterId]);
-		res
-			.status(200)
-			.json(battleField)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(battleField);
+		// .catch((err) => res.status(500).send(err));
 		// console.log(battleField.map((elem) => elem.name));
 	},
 
@@ -90,10 +76,8 @@ module.exports = {
 		battleField.splice(monsterId, 1);
 		//edit code here
 		// console.log(battleField);
-		res
-			.status(200)
-			.json(monsters)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(monsters);
+		// .catch((err) => res.status(500).send(err));
 	},
 
 	deleteBattleField: (req, res, next) => {
@@ -102,10 +86,8 @@ module.exports = {
 			(monster) => ':' + monster.index == deleteId
 		);
 		battleField.splice(monsterId, 1);
-		res
-			.status(200)
-			.json(battleField)
-			.catch((err) => res.status(500).send(err));
+		res.status(200).json(battleField);
+		// .catch((err) => res.status(500).send(err));
 		// console.log(battleField.map((elem) => elem.name));
 	}
 };
